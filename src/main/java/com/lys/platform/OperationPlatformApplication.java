35b802b386f3d5560098d1c33ceae2937c775448
zhangwanquan <412773737@qq.com>
2025-07-26 09:36:46 +0800

    创建项目

---
+++ src/main/java/com/lys/platform/OperationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.lys.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperationPlatformApplication.class, args);
+    }
+
+}
